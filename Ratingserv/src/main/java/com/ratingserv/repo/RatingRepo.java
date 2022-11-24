be2d655d99259585e7850efcd94a93cd1f4bddd5
Sekhar Shrestha <seshrestha17@gmail.com>
2022-11-24 17:34:08 +0530

    ratingserv logic implementation

--- Ratingserv/src/main/java/com/ratingserv/repo/RatingRepo.java
+++ Ratingserv/src/main/java/com/ratingserv/repo/RatingRepo.java
@@ -1,9 +1,14 @@
 package com.ratingserv.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ratingserv.entities.Rating;
 
 public interface RatingRepo extends JpaRepository<Rating, String> {
+	
+	public List<Rating> findByUserId(String userId);
+	public List<Rating> findByHotelId(String hotelId);
 
 }
