7929f8a85a33cc3082439c1a4012603fb2aae5b7
Sekhar Shrestha <seshrestha17@gmail.com>
2022-11-24 16:11:35 +0530

    ratingserv first commit

---
+++ Ratingserv/src/main/java/com/ratingserv/RatingservApplication.java
@@ -0,0 +1,13 @@
+package com.ratingserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingservApplication.class, args);
+	}
+
+}
---
+++ Ratingserv/src/test/java/com/ratingserv/RatingservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratingserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
